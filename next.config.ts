58896623a9a445d6440f1cd5784f9504466ff9f7
chancecarr <chancecarr1@gmail.com>
2026-09-10 11:27:37 -0600

    Change config for static site deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
 };
 
 export default nextConfig;
